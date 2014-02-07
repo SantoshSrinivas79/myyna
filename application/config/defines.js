b7b40060e834fcd94e1b31dee1bb87fd066625e9
cubettech <github@cubettech.com>
2014-02-07 13:48:27 +0530

    installation file

--- application/config/defines.js
+++ application/config/defines.js
@@ -8,29 +8,27 @@
  */
 
 global.DEFINES = {
-    IMAGE_PATH: '/pins/images/',
+    IMAGE_PATH: sleekConfig.siteUrl + '/pins/images/',
     IMAGE_PATH_REL: appPath + '/uploads/pins/images/',
-    IMAGE_PATH_ORIGINAL: '/pins/images/',
+    IMAGE_PATH_ORIGINAL: sleekConfig.siteUrl + '/pins/images/',
     IMAGE_PATH_ORIGINAL_REL: appPath + '/uploads/pins/images/',
-    IMAGE_PATH_SMALL:  '/pins/images/small/',
+    IMAGE_PATH_SMALL: sleekConfig.siteUrl + '/pins/images/small/',
     IMAGE_PATH_SMALL_REL: appPath + '/uploads/pins/images/small/',
-    IMAGE_PATH_THUMB: '/pins/images/thumb/',
+    IMAGE_PATH_THUMB: sleekConfig.siteUrl + '/pins/images/thumb/',
     IMAGE_PATH_THUMB_REL: appPath + '/uploads/pins/images/thumb/',
-    AUDIO_PATH: '/pins/audio/',
+    AUDIO_PATH: sleekConfig.siteUrl + '/pins/audio/',
     AUDIO_PATH_REL: appPath + '/uploads/pins/audio/',
-    VIDEO_PATH:  '/pins/videos/',
+    VIDEO_PATH: sleekConfig.siteUrl + '/pins/videos/',
     VIDEO_PATH_REL: appPath + '/uploads/pins/videos/',
     USER_IMAGE_PATH: appPath + '/uploads/user_images/',
     USER_IMAGE_WIDTH:'140',
-    FB_APP_ID : "442361052535223",
-    FB_APP_SECRET : "08ead50d01bf9ab6eb63662bd0c0784d",
+    FB_APP_ID : "185425611653848",
+    FB_APP_SECRET : "a79dabba32451633acd8e0b7bf60729b",
     TW_CONS_KEY : "DJkq7VoVqzDPyLGRaaC5ig",
     TW_CONS_SECRET : "Qr5hDuqKhXUkSt0ZaqaLqGhEC4GWpImAfOfFyWXB0tQ",
     TW_LOGIN : true,
-    FB_LOGIN : true
-//    PAYPAL_HOST : "api.sandbox.paypal.com",
-//    PAYPAL_CLIENT_ID : "ATi9DBCq9BHrOKM7sSGLpqIboLO_BFV8JW59JJXxDQwbsezoC1ssNwDLZkOQ",
-//    PAYPAL_CLIENT_SECRET : "EGYv9BC7M9Nz99DLeGo_gVt6Y3PzDEBZGt5yh0dmcqF0FL_ZBePV_jOteX",
+    FB_LOGIN : true,
+//site_logo : sleekConfig.siteUrl + '/default/cubet_board.png'
 
 }
- //Last updated by admin user on Tue Jan 28 2014 13:47:27 GMT+0530 (IST)
+ //Last updated by admin user on Mon Feb 03 2014 13:35:12 GMT+0000 (UTC)
